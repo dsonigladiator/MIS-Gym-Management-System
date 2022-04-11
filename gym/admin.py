9cc3f0c7e12d2279d5041f93e14e3c4d85df78de
Divyang Soni <DIVYANG.SONI@svkmmumbai.onmicrosoft.com>
2022-04-12 00:02:02 +0530

    Delete gym directory

--- gym/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Plan,Enquiry, Equipment, Member
-
-# Register your models here.
-
-admin.site.register(Plan)
-admin.site.register(Enquiry)
-admin.site.register(Equipment)
-admin.site.register(Member)
